ef95af8e9bc6ab7c9e2e38fe0b0e6c3356d2c464
Oliver <kicks.oliver@gmail.com>
2020-01-24 13:11:36 -0500

    updated routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,13 +8,10 @@ import { CategoryComponent } from './components/category/category.component';
 import { ThreadComponent } from './components/thread/thread.component';
 import { PostComponent } from './components/post/post.component';
 import { CategoryCreateComponent } from './components/category/category-create/category-create.component';
-import { CategoryDeleteComponent } from './components/category/category-delete/category-delete.component';
 import { CategoryEditComponent } from './components/category/category-edit/category-edit.component';
 import { ThreadCreateComponent } from './components/thread/thread-create/thread-create.component';
-import { ThreadDeleteComponent } from './components/thread/thread-delete/thread-delete.component';
 import { ThreadEditComponent } from './components/thread/thread-edit/thread-edit.component';
 import { PostCreateComponent } from './components/post/post-create/post-create.component';
-import { PostDeleteComponent } from './components/post/post-delete/post-delete.component';
 import { PostEditComponent } from './components/post/post-edit/post-edit.component';
 
 const routes: Routes = 
@@ -28,17 +25,14 @@ const routes: Routes =
     [
       { path: 'category', component: CategoryComponent },
       { path: 'category/create', component: CategoryCreateComponent },
-      { path: 'category/delete', component: CategoryDeleteComponent },
       { path: 'category/edit/:id', component: CategoryEditComponent },
 
       { path: 'thread/:catId', component: ThreadComponent },
       { path: 'thread/create/:catId', component: ThreadCreateComponent },
-      { path: 'thread/delete/:id', component: ThreadDeleteComponent },
       { path: 'thread/edit/:catId/:id', component: ThreadEditComponent },
 
       { path: 'post/:thrId', component: PostComponent },
       { path: 'post/create/:thrId', component: PostCreateComponent },
-      { path: 'post/delete/:id', component: PostDeleteComponent },
       { path: 'post/edit/:thrId/:id', component: PostEditComponent },
     ] 
   },
