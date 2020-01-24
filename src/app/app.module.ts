efd01dd40d2c809998b01c096f0db894ce30cdac
Oliver <kicks.oliver@gmail.com>
2020-01-23 23:00:30 -0500

    header done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from "@angular/material";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThreadComponent } from './components/thread/thread.component';
@@ -8,7 +10,7 @@ import { PostComponent } from './components/post/post.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +20,12 @@ import { SignupComponent } from './components/signup/signup.component';
     HomeComponent,
     HeaderComponent,
     LoginComponent,
-    SignupComponent
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
